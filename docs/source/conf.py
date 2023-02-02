ade826370272f8a7229d2e4660d8c6b35c8ff48a
linuxscout <taha_zerrouki@hotmail.com>
2023-02-02 14:28:30 +0100

    Add mkdocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ templates_path = ['wstemplates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = '.md'
 
 # The master toctree document.
 master_doc = 'index'
